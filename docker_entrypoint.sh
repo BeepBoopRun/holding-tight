2f00e0463d1f8ccec1eebc026bec51a23ee09b4a
Ziz <>
2025-08-27 01:02:52 +0200

    Large files require large timeout

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,7 +2,7 @@
 export DJANGO_DEBUG="False"
 python manage.py migrate
 
-gunicorn -b 0.0.0.0:8000 ligand_service.wsgi --timeout 90 &
+gunicorn -b 0.0.0.0:8000 ligand_service.wsgi --timeout 120 &
 python manage.py run_huey
 
 wait
