920b26a4282045a1322453e265cd5c684a0d00e0
Ziz <>
2025-09-02 01:13:15 +0200

    reorganized structure

--- ligand_service/contacts.py
+++ ligand_service/contacts.py
@@ -2,7 +2,7 @@ import sys
 import os
 import subprocess as sb
 from pathlib import Path
-from typing import Any, NamedTuple
+from typing import Any
 import tempfile
 import re
 
@@ -51,12 +51,6 @@ THREE_TO_ONE = {
 
 ONE_TO_THREE = {v: k for k, v in THREE_TO_ONE.items()}
 
-
-class VMDFiles(NamedTuple):
-    topology: Path
-    trajectory: Path
-
-
 def filetype(file: Path) -> str:
     filetype = file.suffix[1:]
     if filetype == "cms":
@@ -80,59 +74,6 @@ def get_sequence_chains(
             structure[chain] = {}
         structure[chain][residue_id] = THREE_TO_ONE.get(resname, "X")
     return structure
-
-
-def get_files_maestro(directory: Path) -> VMDFiles | None:
-    print(f"Directory: {directory}")
-
-    top_candidates: list[Path] = []
-    trj_candidates: list[Path] = []
-
-    for file in directory.rglob("*"):
-        if file.suffix == ".cms":
-            top_candidates.append(file)
-        elif file.suffix == ".dtr":
-            trj_candidates.append(file)
-
-    if not top_candidates or not trj_candidates:
-        print("Needed files not found in given directory!")
-        return None
-
-    chosen_top = None
-    chosen_trj = None
-
-    for candidate in top_candidates:
-        if candidate.name.endswith("out.cms"):
-            chosen_top = candidate
-            break
-    if chosen_top is None:
-        chosen_top = sorted(top_candidates)[0]
-
-    for candidate in trj_candidates:
-        if candidate.name == "clickme.dtr":
-            chosen_trj = candidate
-            break
-    if chosen_trj is None:
-        chosen_trj = sorted(trj_candidates)[0]
-
-    if chosen_top is None or chosen_trj is None:
-        return None
-
-    return VMDFiles(chosen_top, chosen_trj)
-
-
-def get_files_dir(directory: Path) -> VMDFiles | None:
-    top = None
-    trj = None
-    for file in directory.iterdir():
-        if file.suffix in [".pdb", ".psf"] and top is None:
-            top = file
-        elif file.suffix in [".dcd", ".xtc", ".trr"] and trj is None:
-            trj = file
-        if top is not None and trj is not None:
-            return VMDFiles(top, trj)
-    return None
-
 
 def get_interactions(topology_file: Path, trajectory_file: Path, outfile: Path):
     process = sb.Popen(
@@ -315,7 +256,7 @@ def extract_uniprot_ident(target_description: str) -> str:
     return target_description.split("|")[1]
 
 
-def create_translation_dict_by_vmd(
+def create_translation_dict_by_blast(
     topology: Path, trajectory: Path
 ) -> dict[tuple[str, str, str], str] | None:
     seq_chains = get_sequence_chains(topology, trajectory)
--- ligand_service/models.py
+++ ligand_service/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 import uuid
+from pathlib import Path
+from typing import NamedTuple
 
 
 class Submission(models.Model):
@@ -11,6 +14,13 @@ class Submission(models.Model):
     email = models.EmailField(null=True, blank=True, default=None)
     common_numbering = models.BooleanField()
 
+    def get_main_directory(self):
+        return Path(settings.MEDIA_ROOT).joinpath(str(self.id))
+
+    def get_results_directy(self):
+        return self.get_main_directory().joinpath("results")
+
+        
 
 class SubmittedForm(models.Model):
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
@@ -23,6 +33,19 @@ class SubmittedForm(models.Model):
         TOPTRJ_PAIR = "T", "TopTrjPair"
 
     file_input = models.CharField(max_length=1, choices=FILE_INPUT_TYPES)
+
+    def get_main_directory(self):
+        return self.submission.get_main_directory().joinpath(str(self.form_id))
+
+    def get_trajectory_files(self):
+        if self.file_input == SubmittedForm.FILE_INPUT_TYPES.MAESTRO_DIR:
+            files = get_files_maestro(self.get_main_directory())
+        elif self.file_input == SubmittedForm.FILE_INPUT_TYPES.TOPTRJ_PAIR:
+            files = get_files_dir(self.get_main_directory())
+        else:
+            # unreachable
+            assert False
+        return files
 
 
 class SubmissionTask(models.Model):
@@ -46,3 +69,58 @@ class SubmissionTask(models.Model):
 class GPCRdbResidueAPI(models.Model):
     uniprot_identifier = models.CharField(max_length=12)
     response_json = models.JSONField()
+
+
+class TrajectoryFiles(NamedTuple):
+    topology: Path
+    trajectory: Path
+
+
+def get_files_maestro(directory: Path) -> TrajectoryFiles | None:
+    top_candidates: list[Path] = []
+    trj_candidates: list[Path] = []
+
+    for file in directory.rglob("*"):
+        if file.suffix == ".cms":
+            top_candidates.append(file)
+        elif file.suffix == ".dtr":
+            trj_candidates.append(file)
+
+    if not top_candidates or not trj_candidates:
+        print("Needed files not found in given directory!")
+        return None
+
+    chosen_top = None
+    chosen_trj = None
+
+    for candidate in top_candidates:
+        if candidate.name.endswith("out.cms"):
+            chosen_top = candidate
+            break
+    if chosen_top is None:
+        chosen_top = sorted(top_candidates)[0]
+
+    for candidate in trj_candidates:
+        if candidate.name == "clickme.dtr":
+            chosen_trj = candidate
+            break
+    if chosen_trj is None:
+        chosen_trj = sorted(trj_candidates)[0]
+
+    if chosen_top is None or chosen_trj is None:
+        return None
+
+    return TrajectoryFiles(chosen_top, chosen_trj)
+
+
+def get_files_dir(directory: Path) -> TrajectoryFiles | None:
+    top = None
+    trj = None
+    for file in directory.iterdir():
+        if file.suffix in [".pdb", ".psf"] and top is None:
+            top = file
+        elif file.suffix in [".dcd", ".xtc", ".trr"] and trj is None:
+            trj = file
+        if top is not None and trj is not None:
+            return TrajectoryFiles(top, trj)
+    return None
