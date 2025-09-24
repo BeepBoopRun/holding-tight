8071843260fc99ead2e67a3b599277a305b0800c
Ziz <>
2025-09-24 10:32:07 +0200

    csrf removed

--- ligand_service/settings.py
+++ ligand_service/settings.py
@@ -34,10 +34,6 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-]
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -69,7 +65,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
