f7cabaaf5b38ae3c25bb17b823cbef418fe62c95
Ziz <>
2025-10-02 19:21:38 +0200

    before server

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,7 +2,6 @@
 export DJANGO_DEBUG="False"
 
 ./wait-for-it.sh db:5432
-echo "DROP SCHEMA public CASCADE; CREATE SCHEMA public;" | python manage.py dbshell
 python manage.py migrate --no-input
 python manage.py tailwind install --no-input
 python manage.py tailwind build --no-input
--- staticfiles/ligand_service/dashboard.js
+++ staticfiles/ligand_service/dashboard.js
@@ -1,198 +1,208 @@
-function uploadThis(value) {
-	resumables[value].upload();
-	console.log("Started upload!");
+const refreshSpinner = document.getElementById('refreshSpinner');
+const simsContainer = document.getElementById('simsContainer')
+const selectedDirInfo = document.getElementById('filesInfo');
+const confirmButton = document.getElementById("confirmButton");
+const uploadStatusIndicator = document.getElementById("progressNumerical");
+const inputTypeSelect = document.getElementById("inputTypeSelect");
+const cancelButton = document.getElementById("cancelButton");
+const browseButton = document.getElementById('browseButton');
+const clearButton = document.getElementById('clearButton');
+
+
+async function updateSimsData() {
+	animate_class = Array.from(refreshSpinner.classList).filter((c) => c.startsWith('animate'))[0];
+	refreshSpinner.classList.remove(animate_class);
+	void refreshSpinner.offsetWidth;
+	await new Promise(r => setTimeout(r, 20));
+	refreshSpinner.classList.add(animate_class);
+	await new Promise(r => setTimeout(r, 50));
+	let response = await fetch("api/sims-data", {});
+	let newHTML = await response.text();
+	if (simsContainer.innerHTML !== newHTML) {
+		simsContainer.innerHTML = newHTML;
+		prepareSimContainers();
+		return
+	}
+	await new Promise(r => setTimeout(r, 50));
+	response = await fetch("api/sims-data", {});
+	newHTML = await response.text();
+	if (simsContainer.innerHTML !== newHTML) {
+		simsContainer.innerHTML = newHTML;
+		prepareSimContainers();
+	}
+}
+
+async function deleteSim(sim_name) {
+	const response = fetch("api/sim/delete", {
+		method: "POST",
+		body: JSON.stringify({ sim_name: sim_name }),
+	});
+	await updateSimsData();
 }
 
 
-
-window.addEventListener('DOMContentLoaded', () => {
-
-	var r = new Resumable({
-		target: 'api/file',
-		minFileSizeErrorCallback: function(file, errorCount) { },
-		// testChunks: false,
+async function startSim(sim_name) {
+	const response = fetch("api/sim/start", {
+		method: "POST",
+		body: JSON.stringify({ sim_name: sim_name }),
 	});
+	await updateSimsData();
+}
 
-	r.assignDrop(document.getElementById('browseButton'));
-	r.assignBrowse(document.getElementById('browseButton'), true);
-	r.on('fileAdded', function(file, event) {
-		console.log(file)
+function getSimName(any_inner_element) {
+	console.log(any_inner_element)
+	const simData = any_inner_element.closest('.sim-data');
+	return simData.getElementsByClassName('sim-name')[0].innerText;
+}
 
-	});
-	const browseButton = document.getElementById("browseButton")
-	const filesInfoElement = document.getElementById("filesInfo")
-	const confirmButton = document.getElementById("confirmButton")
-	const inputTypeSelect = document.getElementById("inputTypeSelect")
-	const uploadStatusIndicator = document.getElementById("uploadStatus")
-	r.on('progress', function() {
-		uploadStatusIndicator.innerText = (r.progress() * 100).toPrecision(4) + "%"
-		uploadStatusIndicator.classList.remove("invisible")
-	});
-	confirmButton.addEventListener("click", (event) => {
-		const fileCount = resumables[0].files.length
-		console.log(fileCount)
-		resumables[0].opts.query = { 'fileCount': fileCount }
-		console.log(resumables[0])
-		resumables[0].upload()
-	})
 
-	function deleteSim(sim_name) {
-		const response = fetch("https://example.org/post", {
-			method: "POST",
-			body: JSON.stringify({ sim_name: sim_name }),
-		});
-		// update sim_data
-	}
+function prepareSimContainers() {
+	const simContainers = document.getElementsByClassName("sim-data");
 
-	function getSimName(any_inner_element) {
-		const simContainer = any_inner_element.closest('.sim-container')
-		return simContainer.getElementsByClassName('sim-name')[0].innerText
-	}
-
-	function prepareSimContainers() {
-		const simContainers = document.getElementsByClassName("sim-container")
-
-		simContainers.forEach((x) => {
-			const deleteBtn = x.getElementsByClassName('delete-sim-btn')[0];
-			const simName = getSimName(deleteBtn)
+	Array.from(simContainers).forEach((x) => {
+		const deleteBtn = x.getElementsByClassName('delete-sim-btn')[0];
+		const simName = getSimName(deleteBtn);
+		if (deleteBtn != null) {
 			deleteBtn.addEventListener("click", () => {
-				deleteSim(simName)
+				console.log('got event, requesting delete...');
+				deleteSim(simName);
 			});
-		})
-	}
+		}
 
+		const startSimBtn = x.getElementsByClassName('run-sim-btn')[0];
+		if (startSimBtn != null) {
+			startSimBtn.addEventListener("click", () => {
+				console.log('got event, requesting start...');
+				startSim(simName);
+			});
+
+		}
+
+		const showResultsBtn = x.getElementsByClassName('show-results-btn')[0];
+		if (showResultsBtn != null) {
+			showResultsBtn.addEventListener("click", () => {
+				console.log('got event, showing results...');
+				location.href = `/show/${showResultsBtn.dataset.simId}`
+			});
+		}
+
+	})
+}
+
+
+refreshSpinner.addEventListener("click", async (event) => {
+	await updateSimsData();
 });
 
-let nextFileFormID = 1
 
-function updateFormsetIDs(containingElement, newID) {
-	const re = /submit-\d+|submit-__prefix__/
-	const replaceText = `submit-${newID}`
-	const allDescendants = containingElement.querySelectorAll("*")
-	for (const desc of allDescendants) {
-		if (desc.id) {
-			desc.id = desc.id.replace(re, replaceText)
-		}
-		if (desc.htmlFor) {
-			desc.htmlFor = desc.htmlFor.replace(re, replaceText)
-		}
-		if (desc.name) {
-			desc.name = desc.name.replace(re, replaceText)
-		}
-	}
+var r = new Resumable({
+	target: 'api/sim/upload',
+	minFileSizeErrorCallback: function(file, errorCount) { },
+	// testChunks: false,
+	maxFilesErrorCallback: function(files, errorCount) {
+		alert('Choose two files: topology and trajectory');
+	},
+	testChunks: false,
+});
+
+
+function getFileMainDirectory(file) {
+	return file.relativePath.split('/')[0]
 }
 
-function addFiles() {
-	const template = document.getElementById('file-input-template');
-	const form = document.getElementById('file-table');
-	const add_more_btn = document.getElementById('add-more-btn');
-	currentPrefix = nextFileFormID.toString();
-	nextFileFormID += 1
-
-	for (const elem of template.children) {
-		clone = elem.cloneNode(true)
-		updateFormsetIDs(clone, currentPrefix)
-		form.insertBefore(clone, add_more_btn);
+function displayCurrentFiles() {
+	if (selectedInput === "topTrj") {
+		selectedDirInfo.innerText = "Files: " + r.files.reduce((acc, val) => acc + " " + val.fileName, "");
+	} else {
+		selectedDirInfo.innerText = `Directory: ${getFileMainDirectory(r.files[0])}`;
 	}
 
-	const formset_handler = document.getElementById('id_submit-TOTAL_FORMS');
-	formset_handler.value = parseInt(formset_handler.value) + 1;
 }
 
-function deleteThisFileForm() {
-	event.target.closest('.entry').remove();
-
-	// contains buttons as well!!
-	const form = document.getElementById('input-form')
-	console.log(form)
-	let allFileForms = Array.from(form.children).filter(child =>
-		child.classList.contains('entry')
-	);
-	console.log(allFileForms);
-	let i = 0;
-	for (const fileForm of allFileForms) {
-		updateFormsetIDs(fileForm, i);
-		i += 1
+r.on('fileAdded', function(file, event) {
+	// ensure that only one directory at most is present in the upload 
+	if (selectedInput === "maestroDir") {
+		console.log('Directory input, making sure oly one dir exists!')
+		const fileMainDir = getFileMainDirectory(file)
+		r.files = r.files.filter((file) => getFileMainDirectory(file) === fileMainDir)
 	}
-	nextFileFormID -= 1;
 
-	const formset_handler = document.getElementById('id_submit-TOTAL_FORMS');
-	formset_handler.value = parseInt(formset_handler.value) - 1;
+	displayCurrentFiles();
+});
+
+function resetResumableFileUploaderState() {
+	selectedInput = inputTypeSelect.value;
+	if (selectedInput === "topTrj") {
+		selectedDirInfo.innerText = "Select files...";
+		r.assignBrowse(browseButton, false);
+		r.opts.maxFiles = 2
+	} else {
+		selectedDirInfo.innerText = "Select directory...";
+		r.assignBrowse(browseButton, true);
+		r.opts.maxFiles = undefined
+	}
+	r.files = []
 }
 
-function handleChangedFileType() {
-	fileLabel = event.target.closest('.entry').querySelector('.file-label');
-	currentOption = event.target.value;
-	fileInput = fileLabel.querySelector('.file-input')
+r.on('complete', function(file, event) {
+	uploadStatusIndicator.classList.add("hidden");
+	confirmButton.classList.add("rounded-r-lg");
+	selectedDirInfo.innerText = "Select directory...";
+	resetResumableFileUploaderState();
+	updateSimsData();
+});
 
-	let newLabelText = undefined;
-	// changing html attributes to match input type,
-	// (maestro -> webkitdir), (toptrj -> multiple files)
-	//
-	// changing label text as well
-	if (currentOption === "MaestroDir") {
-		newLabelText = "Upload directory...";
-		fileInput.removeAttribute("multiple");
-		fileInput.setAttribute("webkitdirectory", "")
-	} else if (currentOption === "TopTrjPair") {
-		newLabelText = "Upload files..."
-		fileInput.setAttribute("multiple", "");
-		fileInput.removeAttribute("webkitdirectory");
+clearButton.addEventListener("click", () => {
+	resetResumableFileUploaderState();
+	r.cancel();
+});
+
+/*
+r.on('filesAdded', function(arrayAdded, arraySkipped) {
+	r.files = arrayAdded
+	displayCurrentFiles();
+});
+*/
+
+inputTypeSelect.addEventListener('change', (event) => {
+	resetResumableFileUploaderState();
+});
+
+
+
+
+confirmButton.addEventListener("click", (event) => {
+	const fileCount = r.files.length;
+	if (fileCount <= 0) {
+		return;
 	}
-
-	// changing the text to match selected input type
-	for (let i = 0; i < fileLabel.childNodes.length; i++) {
-		if (fileLabel.childNodes[i].nodeType === Node.TEXT_NODE) {
-			fileLabel.childNodes[i].nodeValue = newLabelText;
-			break;
+	if (selectedInput === "topTrj") {
+		if (fileCount < 2) {
+			alert('Choose two files: topology and trajectory');
 		}
 	}
 
-	// removing any currently chosen files
-	fileInput.value = "";
-}
+	uploadStatusIndicator.classList.remove("hidden");
+	confirmButton.classList.remove("rounded-r-lg");
 
-function handleNewFiles() {
-	fileInput = event.target;
-	filePathsInput = event.target.closest('.entry').querySelector('.file-paths');
-	fileLabel = event.target.closest('.entry').querySelector('.file-label');
-
-	let chosenFiles = "";
-	if (fileInput.hasAttribute("multiple")) {
-		chosenFiles = 'Chosen files: ' + Array.from(fileInput.files, (x) => x.name).join(', ');
-	} else if (fileInput.hasAttribute("webkitdirectory")) {
-		const paths = {};
-		const dt = new DataTransfer();
-		// we need to replace FileList object to remove
-		// deduplicate file names...
-		let i = 0
-		for (const file of fileInput.files) {
-			const filePath = file.webkitRelativePath;
-			const prefixedFileName = `BROWSER_PREFIX${i}_${file.name}`;
-			paths[prefixedFileName] = filePath;
-			const prefixedFile = new File([file], prefixedFileName);
-			dt.items.add(prefixedFile);
-			i += 1;
-
-			if (chosenFiles === "") {
-				chosenFiles = 'Chosen directory: '.concat(file.webkitRelativePath.split("/")[0]);
-			}
-		}
-		filePathsInput.value = JSON.stringify(paths);
-		fileInput.files = dt.files;
+	r.opts.query = { 'fileCount': fileCount };
+	// naming the directory
+	if (selectedInput === "topTrj") {
+		fileNames = [r.files[0].fileName, r.files[1].fileName].sort()
+		dirName = fileNames[0] + "-" + fileNames[1]
+		r.files.forEach((file) => { file.relativePath = `${dirName}/${file.fileName}` })
 	}
 
-	// setting new name for the label to show loaded files
-	for (let i = 0; i < fileLabel.childNodes.length; i++) {
-		if (fileLabel.childNodes[i].nodeType === Node.TEXT_NODE) {
-			fileLabel.childNodes[i].nodeValue = chosenFiles;
-			break;
-		}
-	}
-}
+	console.log('Starting upload!');
+	r.upload();
+});
 
-function toggleExperimentalValues() {
-	const array = document.querySelectorAll('input.val');
-	add_VOI_button = document.getElementById("id_add_VOI");
-	array.forEach((element) => add_VOI_button.checked ? element.classList.remove("hidden") : element.classList.add("hidden"));
-}
+r.on('progress', function(event) {
+	uploadStatusIndicator.innerText = (r.progress() * 100).toPrecision(4) + "%";
+});
+
+
+
+prepareSimContainers();
+resetResumableFileUploaderState();
+setInterval(updateSimsData, 10000)
